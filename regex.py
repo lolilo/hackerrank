deddd64922f3bf86e8b6e93976eeb4f20cc74c1c
Liana Lo <liana.lo@prezi.com>
2015-04-12 00:44:50 +0200

    parenthesis

--- regex.py
+++ regex.py
@@ -1,7 +1,6 @@
 # refactor out min_length_of_string later
 # keep track of shortest extender as we parse? optimize later
 def find_smallest_string_length(regex_expression, regex_index, previous_concatenation_length, current_concatenation_length):
-    # smallest_string_length = 0 
 
     if regex_index >= len(regex_expression): # reached end of regex pattern
         print previous_concatenation_length, current_concatenation_length
@@ -12,13 +11,13 @@ def find_smallest_string_length(regex_expression, regex_index, previous_concaten
     if ord('a') <= ord(char) <= ord('z'):
         current_concatenation_length += 1
         regex_index += 1
-        print 'in ord, current cocat lenght is', current_concatenation_length
+        print 'in ord, current cocat length is', current_concatenation_length
         return 1 + find_smallest_string_length(regex_expression, regex_index, previous_concatenation_length, current_concatenation_length)
 
     elif char == '*':
         chars_that_star_applies_to = 1
-        # if (regex_index - 1 > 0) and (regex_expression[regex_index - 1] == ')'):
-        #     chars_that_star_applies_to = current_concatenation_length
+        if (regex_index - 1 > 0) and (regex_expression[regex_index - 1] == ')'):
+            chars_that_star_applies_to = current_concatenation_length
         current_concatenation_length = 0
         regex_index += 1
         return (- chars_that_star_applies_to) + find_smallest_string_length(regex_expression, regex_index, previous_concatenation_length, current_concatenation_length)
@@ -33,17 +32,13 @@ def find_smallest_string_length(regex_expression, regex_index, previous_concaten
 
         return min(previous_concatenation_length, current_concatenation_length) - max(previous_concatenation_length, placeholder)
 
+    elif char == '(':
+        new_previous_concatenation_length = 501
+        new_current_concatenation_length = 0
+        regex_index += 1
+        current_concatenation_length += find_smallest_string_length(regex_expression, regex_index, new_previous_concatenation_length, new_current_concatenation_length)
+        return current_concatenation_length
 
-    # elif char == '(':
-    #     new_previous_concatenation_length = 501
-    #     new_current_concatenation_length = 0
-    #     current_concatenation_length += find_smallest_string_length(regex_expression, regex_index + 1, new_previous_concatenation_length, new_current_concatenation_length)
-
-    # elif char == ')':
-    #     return min(previous_concatenation_length, current_concatenation_length)
-
-    # regex_index += 1
-
-    # smallest_string_length = min(previous_concatenation_length, current_concatenation_length)
-    
-
+    elif char == ')':
+        print current_concatenation_length
+        return current_concatenation_length
--- test_regex.py
+++ test_regex.py
@@ -27,15 +27,20 @@ class Test(unittest.TestCase):
         smallest_length = find_smallest_string_length(regex_expression, self.regex_index, self.previous_concatenation_length, self.current_concatenation_length)
         self.assertEqual(smallest_length, 1)
 
-    def test_find_smallest_string_length_or2(self):
+    def test_find_smallest_string_length_or3(self):
         regex_expression = 'c|odsf'
         smallest_length = find_smallest_string_length(regex_expression, self.regex_index, self.previous_concatenation_length, self.current_concatenation_length)
         self.assertEqual(smallest_length, 1)
 
-    # def test_find_smallest_string_length_or(self):
-    #     regex_expression = 'abc*|o'
-    #     smallest_length = find_smallest_string_length(regex_expression, self.regex_index, self.previous_concatenation_length, self.current_concatenation_length)
-    #     self.assertEqual(smallest_length, 1)
+    def test_find_smallest_string_length_parenthesis(self):
+        regex_expression = 'ab*(c|d)'
+        smallest_length = find_smallest_string_length(regex_expression, self.regex_index, self.previous_concatenation_length, self.current_concatenation_length)
+        self.assertEqual(smallest_length, 2)
+
+    def test_find_smallest_string_length_parenthesis2(self):
+        regex_expression = '(ac|vd)*'
+        smallest_length = find_smallest_string_length(regex_expression, self.regex_index, self.previous_concatenation_length, self.current_concatenation_length)
+        self.assertEqual(smallest_length, 2)
 
 if __name__ == "__main__":
     unittest.main()
