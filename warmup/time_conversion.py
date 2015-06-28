ba4b391f7f3a518e7fc3d3ccb7323023370a6a43
Liana Lo <liana.lo@prezi.com>
2015-06-28 20:42:19 +0200

    time conversion cleanup with zfill

--- warmup/time_conversion.py
+++ warmup/time_conversion.py
@@ -1,24 +1,22 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 original_time = raw_input()
+# original_time = '06:40:03AM'
                 
 def is_twelve_hour(time):
     return time[:2] == '12'
 
 def get_converted_hour(orignal_time):
-    orignal_hour = int(original_time[:2])
-    return str(orignal_hour + 12)
+    converted_hour = int(original_time[:2])
+    isAM = original_time[-2] == 'A'
+
+    if not isAM and not is_twelve_hour(orignal_time):
+        converted_hour += 12
+    elif isAM and is_twelve_hour(orignal_time):
+        converted_hour = 0
+
+    return '00' if converted_hour == 0 else str(converted_hour).zfill(2)
 
 def get_converted_time(original_time):
-    isAM = original_time[-2] == 'A'
-    
-    if isAM:
-        if is_twelve_hour(original_time):
-            return '00' + original_time[2:-2]
-        return original_time[:-2]    
-
-    # isPM
-    if is_twelve_hour(original_time):
-        return original_time[:-2]
     converted_hour = get_converted_hour(original_time)
     return converted_hour + original_time[2:-2]
 
