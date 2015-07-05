755cb85275588b12b02b669be12f662da81ed4f3
Liana Lo <liana.lo@prezi.com>
2015-07-05 20:54:56 +0200

    pangrams with Python set.issubset

--- strings/pangrams.py
+++ strings/pangrams.py
@@ -9,7 +9,11 @@ def is_panagram(s, alphabet):
         if len(alphabet) < 1:
             return True
     return False
+
+def is_panagram_easy(s, alphabet): # but more expensive
+    string_set = set(s.lower())
+    return alphabet.issubset(string_set)
     
 alphabet = set(string.ascii_lowercase)
 s = raw_input()
-print 'pangram ' if is_panagram(s, alphabet) else 'not pangram'
+print 'pangram ' if is_panagram_easy(s, alphabet) else 'not pangram'
