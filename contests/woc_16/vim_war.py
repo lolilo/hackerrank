0ed132a6e0f6ef4dc11f965c959f0e9d7dc74956
Liana Lo <liana.lixia@gmail.com>
2015-07-10 00:38:16 +0200

    some vim_war shit - segmentation fault everywhere

--- contests/woc_16/vim_war.py
+++ contests/woc_16/vim_war.py
@@ -1,15 +1,18 @@
-from itertools import chain, combinations
+# from itertools import chain, combinations
 
 
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+# def powerset(iterable):
+#     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+#     s = list(iterable)
+#     return combinations(s, r) for r in range(len(s)+1)
 
 
-def subsets(s):
-    return map(set, powerset(s))
+# def subsets(s):
+#     return map(set, powerset(s))
 
+import itertools
+def findsubsets(S,m):
+    return set(itertools.combinations(S, m))
 
 def is_valid_skillset(skillset_requirement, total_skillset, num_possible_skills):
     for i in xrange(num_possible_skills):
@@ -24,25 +27,27 @@ def is_valid_army(army, soldiers_array, skillset_requirement, num_possible_skill
         for skill in xrange(num_possible_skills):
             if soldiers_array[soldier_num][skill] == 1:
                 total_skillset[skill] = 1
+
     return is_valid_skillset(skillset_requirement, total_skillset, num_possible_skills)
 
 
 def get_valid_armies(num_soldiers, soldiers_array, skillset_requirement, num_possible_skills):
-    possible_armies = subsets(range(num_soldiers))
     valid_army_count = 0
-    for army in possible_armies:
-        if is_valid_army(army, soldiers_array, skillset_requirement, num_possible_skills):
-            valid_army_count += 1
+    for length in xrange(num_soldiers + 1):
+        possible_armies = findsubsets(range(num_soldiers), length)
+        for army in possible_armies:
+            if is_valid_army(army, soldiers_array, skillset_requirement, num_possible_skills):
+                valid_army_count += 1
     return valid_army_count
 
 
 def hackerrank():
     num_soldiers, num_possible_skills = [int(x) for x in raw_input().split()]
     soldiers_array = []
-    for _ in xrange(num_solders):
-        skill_to_soldier_array = [int(x) for x in raw_input().split()]
+    for _ in xrange(num_soldiers):
+        skill_to_soldier_array = [int(x) for x in list(raw_input())]
         soldiers_array.append(skill_to_soldier_array)
-    skillset_requirement = [int(x) for x in raw_input().split()]
+    skillset_requirement = [int(x) for x in list(raw_input())]
     print get_valid_armies(num_soldiers, soldiers_array, skillset_requirement, num_possible_skills)
 
 
