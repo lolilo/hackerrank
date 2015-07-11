a05ef556d5f6497032cc8d063ff3298e28f951b8
Liana Lo <liana.lo@prezi.com>
2015-07-11 12:54:37 +0200

    slow recursive approach

--- contests/july_2015/new_york_life/count_strings.py
+++ contests/july_2015/new_york_life/count_strings.py
@@ -1,7 +1,7 @@
 # Complete the function below.
 
 def get_num_valid_strings(l, target_value, alphabet_value_array, alphabet_value_set):
-    if l < 1 and target_value > 0:
+    if (l < 1 and target_value > 0) or target_value < 0:
         return 0
 
     if l == 0 and target_value == 0:
@@ -9,15 +9,12 @@ def get_num_valid_strings(l, target_value, alphabet_value_array, alphabet_value_
 
     count = 0
     for value in alphabet_value_array:
-        remaining_value = target_value - value
-        if remaining_value in alphabet_value_set: # this is wrong. We need to see if there exists numbers that add up to remaining value
+        if value <= target_value:
+            remaining_value = target_value - value
             count += get_num_valid_strings(l - 1, target_value - value, alphabet_value_array, alphabet_value_set)
     return count
 
 def countStrings(l, v):
-    alphabet_value_array = [0] + [x**2 for x in xrange(1, 27)]
+    alphabet_value_array = [x**2 for x in xrange(1, 27)]
     alphabet_value_set = set(alphabet_value_array)
     return get_num_valid_strings(l, v, alphabet_value_array, alphabet_value_set)
-
-count = countStrings(2, 13)
-print count
--- contests/july_2015/new_york_life/test_count_strings.py
+++ contests/july_2015/new_york_life/test_count_strings.py
@@ -7,19 +7,27 @@ import itertools
 class RegexTest(unittest.TestCase):
     def setUp(self):
         self.alphabet = set(string.ascii_lowercase)
-        self.alphabet_value_array = [0] + [x**2 for x in xrange(1, 27)]
+        self.alphabet_value_array = [x**2 for x in xrange(1, 27)]
         # self.alphabet_value_array = [4, 9]
         self.alphabet_value_set = set(self.alphabet_value_array)
 
     def test_countStrings(self):
         count = countStrings(2, 13)
         self.assertEqual(count, 2)
+        count = countStrings(3, 29)
+        self.assertEqual(count, 6)
 
     def test_get_num_valid_strings(self):
         l = 2
         target_value = 13
         count = get_num_valid_strings(l, target_value, self.alphabet_value_array, self.alphabet_value_set)
         self.assertEqual(count, 2)
+
+    def test_get_num_valid_strings2(self):
+        l = 3
+        target_value = 29
+        count = get_num_valid_strings(l, target_value, self.alphabet_value_array, self.alphabet_value_set)
+        self.assertEqual(count, 6)
 
 if __name__ == "__main__":
     unittest.main()
