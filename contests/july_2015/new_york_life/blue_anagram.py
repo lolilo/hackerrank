5820cb0d79a0f9d7cbfd4829980aeef27f601ded
Liana Lo <liana.lo@prezi.com>
2015-07-11 10:50:48 +0200

    can improve anagram function

--- contests/july_2015/new_york_life/blue_anagram.py
+++ contests/july_2015/new_york_life/blue_anagram.py
@@ -9,25 +9,47 @@ def make_dict(str):
             d[char] = 1
     return d
 
-def is_anagram(word1, word2):
-    d1 = make_dict(word1)
-    d2 = make_dict(word2)
+def is_anagram(word):
+    d1 = {'E': 1, 'B': 1, 'U': 1, 'L': 1}
+    d2 = make_dict(word)
     for key in d2.keys():
         if not d1.get(key) or d1[key] != d2[key]:
             return False
     return True
 
-def make_x(word):
-    return 'X' * len(word)
+def replace_anagram_with_x(start_index, str, str_len):
+    word = ''
+    index = start_index
+    while index < str_len and str[index] != ' ':
+        word += str[index]
+        index += 1
+    if is_anagram(word):
+        make_x(start_index, index, str)
+    return str, index
+
+def make_x(start_index, end_index, str):
+    index = start_index
+    while index < end_index:
+        str[index] = 'X'
+        index += 1
+    return str
     
 def replaceBLUE(str):
-    new_str = ''
-    words = str.split()
-    for word in words:
-        if is_anagram('BLUE', word):
-            new_str += make_x(word)
-        else:
-            new_str += word
-        new_str += ' '
-    return new_str
-        
+    str = list(str)
+    str_len = len(str)
+    char_index = 0
+    while char_index < str_len:
+        char = str[char_index]
+        if char == ' ':
+            char_index += 1
+            continue
+        str, end_index = replace_anagram_with_x(char_index, str, str_len)
+        char_index = end_index
+    return ''.join(str)
+
+str = '   GET    BLUEYO RID OF BLUE AND UEBL BUT LEBUS ARE GOOD    '
+str = ' BL     '
+print replaceBLUE(str)
+print len(replaceBLUE(str))
+
+
