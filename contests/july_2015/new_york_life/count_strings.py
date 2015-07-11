b8067dd8d31bb3a5c126e5f48a45dc077c8590a4
Liana Lo <liana.lo@prezi.com>
2015-07-11 12:32:24 +0200

    faulty recursive approach

--- contests/july_2015/new_york_life/count_strings.py
+++ contests/july_2015/new_york_life/count_strings.py
@@ -1,47 +1,23 @@
 # Complete the function below.
-import string
-import itertools
 
-def factorial(n):
-    factorial = 1
-    for n in xrange(1, n+1):
-        factorial *= n
-    return factorial
+def get_num_valid_strings(l, target_value, alphabet_value_array, alphabet_value_set):
+    if l < 1 and target_value > 0:
+        return 0
 
-def nPr(n, r):
-    numerator = factorial(n)
-    denominator = factorial(n-r)
-    return numerator/denominator
+    if l == 0 and target_value == 0:
+        return 1
 
-def findsubsets(alphabet, n):
-    return itertools.combinations(alphabet, n)
-
-def get_subset_value(alphabet_value_array, subset):
-    value = 0
-    for char in subset:
-        char_position = ord(char) - 96
-        value += alphabet_value_array[char_position]
-    return value
+    count = 0
+    for value in alphabet_value_array:
+        remaining_value = target_value - value
+        if remaining_value in alphabet_value_set: # this is wrong. We need to see if there exists numbers that add up to remaining value
+            count += get_num_valid_strings(l - 1, target_value - value, alphabet_value_array, alphabet_value_set)
+    return count
 
 def countStrings(l, v):
     alphabet_value_array = [0] + [x**2 for x in xrange(1, 27)]
-    alphabet = set(string.ascii_lowercase)
-    alphabet_array = []
-    for letter in alphabet:
-        alphabet_array.extend([letter] * l)
-    subsets = findsubsets(alphabet_array, l)
-    count = 0
-    count_increment = nPr(l, l)
-    valid_subsets = set()
-    for subset in subsets:
-        subset_value = get_subset_value(alphabet_value_array, subset)
-        if subset_value == v:
-            if subset in valid_subsets:
-                continue
-            count += count_increment
-            valid_subsets.add(subset)
-    return count
-
+    alphabet_value_set = set(alphabet_value_array)
+    return get_num_valid_strings(l, v, alphabet_value_array, alphabet_value_set)
 
 count = countStrings(2, 13)
 print count
--- contests/july_2015/new_york_life/test_count_strings.py
+++ contests/july_2015/new_york_life/test_count_strings.py
@@ -8,13 +8,17 @@ class RegexTest(unittest.TestCase):
     def setUp(self):
         self.alphabet = set(string.ascii_lowercase)
         self.alphabet_value_array = [0] + [x**2 for x in xrange(1, 27)]
-
-    def test_findsubsets(self):
-        subsets = findsubsets(self.alphabet, 2)
-        self.assertEqual(len(set(subsets)), 325)
+        # self.alphabet_value_array = [4, 9]
+        self.alphabet_value_set = set(self.alphabet_value_array)
 
     def test_countStrings(self):
         count = countStrings(2, 13)
+        self.assertEqual(count, 2)
+
+    def test_get_num_valid_strings(self):
+        l = 2
+        target_value = 13
+        count = get_num_valid_strings(l, target_value, self.alphabet_value_array, self.alphabet_value_set)
         self.assertEqual(count, 2)
 
 if __name__ == "__main__":
