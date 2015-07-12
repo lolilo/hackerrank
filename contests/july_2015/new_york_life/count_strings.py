f35f20994c1790fbc705d64769e8444f7bf38449
Liana Lo <liana.lo@prezi.com>
2015-07-12 17:33:48 +0200

    too slow still =/

--- contests/july_2015/new_york_life/count_strings.py
+++ contests/july_2015/new_york_life/count_strings.py
@@ -1,24 +1,24 @@
 def populate_table(l, value, array):
     table = [None for x in xrange(value + 1)]
-    table[0] = [[]]
+    table[0] = [0]
     for i in xrange(1, value+1):
         for num in array:
             if num > i:
+                print num
                 break # numbers are sorted asending
             else:
                 if table[i-num] != None:
-                    new_combination_pairs = table[i-num][:]
-                    for new_combination in new_combination_pairs:
-                        to_add = new_combination + [num]
-                        len_to_add = len(to_add)
-                        if (i < value and len_to_add <= l) or (i == value and len_to_add == l):
+                    num_possible_add_ons = table[i-num] # list of lengths
+                    for length in num_possible_add_ons:
+                        len_to_add = length + 1
+                        if (i < value and len_to_add < l) or (i == value and len_to_add == l):
                             if table[i] == None:
-                                table[i] = [to_add]
+                                table[i] = [len_to_add]
                             else:
-                                table[i].append(to_add)
+                                table[i].append(len_to_add)
 
     combinations = table[value]
-    return len(combinations)
+    return len(combinations) % 10000007
 
 def countStrings(l, v):
     alphabet_value_array = [x**2 for x in xrange(1, 27)]
